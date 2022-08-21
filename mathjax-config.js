102c17c3676a8c7cc1079c64ebd4ab366572ea77
IvanMedri <cocarojasjorgeluis@gmail.com>
2022-08-20 21:29:05 -0500

    Agregue formulas con MathJax

---
+++ mathjax-config.js
@@ -0,0 +1,8 @@
+window.MathJax = {
+
+  tex: { tags: 'ams' },
+  CommonHTML: { linebreaks: { automatic: true } },
+  "HTML-CSS": { linebreaks: { automatic: true } },
+  SVG: { linebreaks: { automatic: true } }
+
+};
