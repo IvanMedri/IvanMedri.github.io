0726f07213d0497097a386052ad40745dfb2bbd2
IvanMedri <cocarojasjorgeluis@gmail.com>
2022-08-24 18:13:56 -0500

    Succesfully included a page with math parts

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Include everything in post1.html into the .page element where this code was included. Execute all scripts in post1.html (Necessary to transform math before including it in the main page).
+const contenidos = document.querySelector('.page')
+fetch('/post1.html') 
+.then(res=>res.text())
+.then(data=>{
+	contenidos.innerHTML = data
+	const parser = new DOMParser()
+	const doc = parser.parseFromString(data, 'text/html')
+	eval(doc.querySelector('script').textContent)
+})
+
+
+ 
+
